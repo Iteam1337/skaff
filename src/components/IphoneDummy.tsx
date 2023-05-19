49b7c0024d1bf981f27a222844ad7f335fe7f404
Christian Landgren <christian@landgren.nu>
2023-05-19 15:55:48 +0200

    feat: add iphone dummy as container for the app

---
+++ src/components/IphoneDummy.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+export default function IphoneDummy({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>{children}</View>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#444',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerContainer: {
+    position: 'relative',
+    width: 375,
+    maxWidth: '100vw',
+    height: '90vh',
+    maxHeight: '100vh',
+    borderWidth: 10,
+    borderColor: 'gray',
+    borderRadius: 30,
+    overflow: 'hidden',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+})
