e0cb9a2ee3d0b76a322eca2d512c4a94ca0a5050
Johanna Grahn <johanna.grahn@iteam.se>
2023-05-23 13:38:40 +0200

    WIP profile

--- src/components/SupplierProfile.tsx
+++ src/components/SupplierProfile.tsx
@@ -1,5 +1,5 @@
-import { Text, Button, List, useTheme } from 'react-native-paper'
-import { StyleSheet, SafeAreaView } from 'react-native'
+import { Text, Button, List, useTheme, Divider, Card } from 'react-native-paper'
+import { StyleSheet, SafeAreaView, View } from 'react-native'
 import { Tabs, TabScreen } from 'react-native-paper-tabs'
 import { ScrollView } from 'react-native-gesture-handler'
 
@@ -12,25 +12,33 @@ const SupplierProfile = ({
 }) => {
   const theme = useTheme()
   return (
-    <ScrollView>
-      <Tabs
-        uppercase={false}
-        style={{ backgroundColor: theme.colors.surface }}
-        theme={theme}
-      >
-        <TabScreen label="Aktuellt">
-          <SafeAreaView>
-            <Text>Aktuellt</Text>
-          </SafeAreaView>
-        </TabScreen>
-        <TabScreen label="Mina avtal">
-          <Text>Mina avtal</Text>
-        </TabScreen>
-        <TabScreen label="Profil">
-          <Text>Profil</Text>
-        </TabScreen>
-      </Tabs>
-    </ScrollView>
+    <Tabs
+      uppercase={false}
+      style={{ backgroundColor: theme.colors.surface }}
+      theme={theme}
+    >
+      <TabScreen label="Aktuellt">
+        <ScrollView>
+          <List.Accordion title="Anbudsförfrågningar">
+            <Card>
+              <Card.Title title="Test1"></Card.Title>
+            </Card>
+          </List.Accordion>
+          <Divider />
+          <List.Accordion title="Erbjudanden">
+            <Card>
+              <Card.Title title="Mumsiga morötter"></Card.Title>
+            </Card>
+          </List.Accordion>
+        </ScrollView>
+      </TabScreen>
+      <TabScreen label="Mina avtal">
+        <Text>Mina avtal</Text>
+      </TabScreen>
+      <TabScreen label="Profil">
+        <Text>Profil</Text>
+      </TabScreen>
+    </Tabs>
   )
 }
 
