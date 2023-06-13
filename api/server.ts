26da4e516ebc6c825afe1c9ccbe7250eccc201a9
Christian Landgren <christian@landgren.nu>
2023-06-13 11:14:45 +0200

    feat: add push notifications

--- api/server.ts
+++ api/server.ts
@@ -8,6 +8,7 @@ import deals from '../src/data/deals'
 import tenderRequests from '../src/data/tenderRequests'
 import suppliers from '../src/data/suppliers'
 import { categories } from '../src/data/categories'
+import { sendPushNotification } from '../lib/notifications'
 const port = process.env.PORT || 3000
 const app = express()
 const server = createServer(app)
@@ -114,6 +115,23 @@ io.on('connection', (socket) => {
   // DEALS
   socket.on('addDeal', (deal) => {
     state.deals.push(deal)
+
+    state.buyers
+      .filter((b) => b.token)
+      .forEach((buyer) => {
+        sendPushNotification({
+          to: buyer.token,
+          title: 'Nytt erbjudande i Skaff',
+          body: `${deal.product?.name || 'Produkt'} om ${deal.category} från ${
+            deal.supplier.name
+          }`,
+          data: {
+            type: 'deal',
+            id: deal.id,
+          },
+        })
+      })
+
     io.emit('deals', state.deals)
   })
 
@@ -126,6 +144,19 @@ io.on('connection', (socket) => {
   // TENDER REQUESTS
   socket.on('addTenderRequest', (tenderRequest) => {
     state.tenderRequests.push(tenderRequest)
+    state.suppliers
+      .filter((s) => s.token)
+      .forEach((supplier) => {
+        sendPushNotification({
+          to: supplier.token,
+          title: 'Ny förfrågan i Skaff',
+          body: `${tenderRequest.title} från ${tenderRequest.buyer}`,
+          data: {
+            type: 'tenderRequest',
+            id: tenderRequest.id,
+          },
+        })
+      })
     io.emit('tenderRequests', state.tenderRequests)
   })
 
